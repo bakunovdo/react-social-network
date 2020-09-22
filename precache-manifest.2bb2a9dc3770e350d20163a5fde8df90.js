75566dfc828adf8559c582902ab453d7b3f1aaf7
bakunovdo <bakunov.do@gmail.com>
2020-09-22 15:48:57 +0300

    Updates

---
+++ precache-manifest.2bb2a9dc3770e350d20163a5fde8df90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f629f572ce2c1a61b549899a6c7114b",
+    "url": "/react-social-network/index.html"
+  },
+  {
+    "revision": "df29c81f1f46ff5038f5",
+    "url": "/react-social-network/static/css/main.ee056d42.chunk.css"
+  },
+  {
+    "revision": "d2f0b60875eae2090a33",
+    "url": "/react-social-network/static/js/2.8608c73b.chunk.js"
+  },
+  {
+    "revision": "885c0e205ef5e79f4aef448a818ba6a1",
+    "url": "/react-social-network/static/js/2.8608c73b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df29c81f1f46ff5038f5",
+    "url": "/react-social-network/static/js/main.858f9efd.chunk.js"
+  },
+  {
+    "revision": "ace55b50b75e7e6fcf12",
+    "url": "/react-social-network/static/js/runtime-main.c7d46465.js"
+  },
+  {
+    "revision": "601989ff441ddc92bc0fa2e05040f9e7",
+    "url": "/react-social-network/static/media/background-profile.601989ff.jpg"
+  }
+]);
